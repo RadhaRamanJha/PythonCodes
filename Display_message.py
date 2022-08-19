cd1c6eab71bab62ab5581e271571b7f7d73f467a
Radha Raman Jha <radharamanjha1989@gmail.com>
2022-08-19 21:19:38 +0530

    Adding new codes

---
+++ Display_message.py
@@ -0,0 +1,4 @@
+def display_message(topic):
+    print(f"I am Learing about {topic.title()} in this chapter!")
+
+display_message("founctions")
---
+++ favorite_book.py
@@ -0,0 +1,4 @@
+def favorite_book(Title):
+    print(f"One of my favorite book is {Title}.")
+
+favorite_book("Power of Habits")
