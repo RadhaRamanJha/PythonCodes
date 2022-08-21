a03441a8cfec8e4f902ea2a319965e056d2785d6
Radha Raman Jha <radharamanjha1989@gmail.com>
2022-08-21 19:54:42 +0530

    <founction_module_codes>

---
+++ calculation.py
@@ -0,0 +1,10 @@
+def calculation():
+    num1 = int(input("Enter first number : "))
+    num2 = int(input("Enter Second number : "))
+    add = num1+num2
+    subst = num1-num2
+    div = num1/num2
+    print(f"The Addition of the two number is : {add}")
+    print(f"The substraction of the two number is : {subst}")
+    print(f"The divison of the two number is : {div}")
+calculation()
---
+++ sum_recursive_function.py
@@ -0,0 +1,9 @@
+import re
+
+
+def recur_addition(n):
+    if n==0:
+        return 0
+    else:
+        return (n + recur_addition(n-1))
+print(f"The Tail Recurrcive addition from 10 to 0 is {recur_addition(10)}")
