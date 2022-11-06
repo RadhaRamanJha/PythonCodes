8494f098c1938eca56e23c34a9dd898d114e4704
Radha Raman Jha <radharamanjha1989@gmail.com>
2022-11-06 21:53:44 +0530

    Class_Inheritance

---
+++ Admin_inheritence.py
@@ -0,0 +1,36 @@
+class User:
+    """ Blue Print for user information Storage"""
+    def __init__(self, first_name,last_name):
+       '''Initialize the user attributes'''
+       self.first_name = first_name
+       self.last_name = last_name
+       self.age = 18
+       self.gender = "Male"
+       self.emailID = "emailID"
+    def describe_user(self):
+        '''Breif description of the User'''
+        print(f"{self.first_name} {self.last_name} is {self.age} year old {self.gender} faculty. You can contact the faculty at {self.emailID}")
+    def greet_user(self):
+        '''Welcome message to user on log in'''
+        print(f"Welcome {self.first_name} {self.last_name}. Glad you logged in !")
+class Admin(User):
+    def __init__(self, first_name, last_name, privileges = ["can add Post","can delte post","can ban user"]):
+        super().__init__(first_name, last_name)
+        self.privileges = privileges
+    def show_privileges(self):
+        print(f"{self.first_name} {self.last_name} is an Admin and has following privileges {self.privileges[:]}")  
+user1 = User("Rahul","Singh")
+user1.age = 29
+user1.gender = "Male"
+user1.emailID = "rahul.sing@imsec.ac.in"
+user1.describe_user()
+user1.greet_user()
+
+user2 = User("Rekha","Goel")
+user2.age = 30
+user2.gender = "Female"
+user2.emailID = "rekha.goel@imsec.ac.in"
+user2.describe_user()
+user2.greet_user()
+admin1 = Admin("Raman","Jha")
+admin1.show_privileges()
