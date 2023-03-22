f4d68f2eef4f2a96c7c3d9f6ce3ae8d5b25249de
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-03-22 23:00:35 +0530

    Update for choice based loop

--- choice_based_list_loop.py
+++ choice_based_list_loop.py
@@ -4,56 +4,66 @@ entered_sequence = []
 class list_operations:
     # filling list 
     def list_filling(self):
-        current_number = int(input("Enter a number: "))
-        if (current_number % 2 == 0):
-            even_list.append(current_number)
-            entered_sequence.append(current_number)
-            print("The updated even number list is")
-            print(even_list)
-        elif (current_number % 2 != 0):
-            odd_list.append(current_number)
-            entered_sequence.append(current_number)
-            print("The updated odd number list is")
-            print(odd_list)
+        number = int(input("Enter a number for filling list: "))
+        if (number % 2 == 0):
+            self.list_append(even_list,number)
+        elif (number % 2 != 0):
+            self.list_append(odd_list,number)
+        self.continue_query()
+    
+    # Query for continuing list filling
+    def continue_query(self):
         continue_query = input("Do you want to enter another number (y/n)")
         if continue_query == 'y':
             self.list_filling()
         else:
             self.user_choice_entry()
-    # even number popping
+    
+    # List appending operation
+    def list_append(self,concrened_list, current_number):
+        concrened_list.append(current_number)
+        entered_sequence.append(current_number)
+
+    # Obtain last entered even number
     def even_number_popping(self):
-        poped_even_number = even_list.pop()
         print("The last entered even number is :- ")
-        print(poped_even_number)
-        print("Updated Even number list is: ", end=" ")
-        print(even_list)
+        self.number_popping(even_list)
         self.user_choice_entry()
-    # odd number popping
+
+    # Obtain last entered odd number
     def odd_number_popping(self):
-        poped_odd_number = odd_list.pop()
-        print(poped_odd_number)
-        print("Updated odd number list is: ", end=" ")
-        print(odd_list)
+        print("The last entered odd number is :- ")
+        self.number_popping(odd_list)
         self.user_choice_entry()
-    # entered sequence display getting error in it
+
+    # pop the list and return last entered number    
+    def number_popping(self,concerned_list):
+        if (len(concerned_list) != 0):
+            poped_number = concerned_list.pop()
+            print(poped_number)
+            if poped_number in entered_sequence:
+                entered_sequence.remove(poped_number)
+        else:
+            print("No numbers left in updated list to Display")
+            exit()
+    
+    # Display sequence of last number entery
     def entered_sequence_display(self):
-        last_entered_number = entered_sequence.pop()
-        while (last_entered_number == self.even_list) or (last_entered_number == self.odd_list) :
-            last_entered_number = entered_sequence.pop()
-            break
-        print(last_entered_number)
-        print("updated sequence of entry is: ",end=" ")
-        print(entered_sequence)
+        if (len(entered_sequence) != 0):
+            last_entered_number = entered_sequence[-1]
+            print(f"The entered number is {last_entered_number}")
+            entered_sequence.remove(last_entered_number)
+        else:
+            print("All the sequence of entered number has been displayed")
         self.user_choice_entry()
-    # taking user choice to run fontion (2 times asking enter a number for the first time ???)
+
+    # Obtaining user choice which fountion to execute
     def user_choice_entry(self):
-        print("press 0 anytime to quit")
-        self.choice = int(input("Enter your choice among (1/2/3/4) to continue: "))
+        self.choices_display()
         while True:
             if (self.choice == 0):
                 break
             elif (self.choice == 1):
-                print("Enter a number: ")
                 self.list_filling()
             elif (self.choice == 2):
                 self.even_number_popping()        
@@ -61,5 +71,19 @@ class list_operations:
                 self.odd_number_popping()
             elif (self.choice == 4):
                 self.entered_sequence_display()
+            else:
+                print("Please Choose among the choice provied")
+        
+    # Choice presenter
+    def choices_display(self):
+        print("\tpress 0 anytime to quit\n")
+        print("""
+             Press 1 to enter a number
+             Press 2 to pop and know last even number
+             Press 3 to pop and know last odd number
+             Press 4 determine the entered sequence of nummbers
+             """)    
+        self.choice = int(input("Enter your choice among (1/2/3/4) to continue: "))
+
 llo = list_operations()
 llo.user_choice_entry()
