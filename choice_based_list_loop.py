46de83fe59015ee96d5c75986199b9d623577c9a
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-03-24 01:02:18 +0530

    updated choice_based_list_loop

--- choice_based_list_loop.py
+++ choice_based_list_loop.py
@@ -1,19 +1,23 @@
-even_list = []
-odd_list = []
-entered_sequence = []
 class list_operations:
+    # constructor
+    def __init__(self):
+        self.even_list = []
+        self.odd_list = []
+        self.entered_sequence = []
+        self.poped_numbers = []
+
     # filling list 
     def list_filling(self):
         number = int(input("Enter a number for filling list: "))
         if (number % 2 == 0):
-            self.list_append(even_list,number)
+            self.list_append(self.even_list,number)
         elif (number % 2 != 0):
-            self.list_append(odd_list,number)
+            self.list_append(self.odd_list,number)
         self.continue_query()
     
     # Query for continuing list filling
     def continue_query(self):
-        continue_query = input("Do you want to enter another number (y/n)")
+        continue_query = input("Do you want to enter another number (y/n): ")
         if continue_query == 'y':
             self.list_filling()
         else:
@@ -22,45 +26,46 @@ class list_operations:
     # List appending operation
     def list_append(self,concrened_list, current_number):
         concrened_list.append(current_number)
-        entered_sequence.append(current_number)
+        self.entered_sequence.append(current_number)
 
     # Obtain last entered even number
     def even_number_popping(self):
         print("The last entered even number is :- ")
-        self.number_popping(even_list)
-        self.user_choice_entry()
+        print(self.number_popping(self.even_list))
 
     # Obtain last entered odd number
     def odd_number_popping(self):
         print("The last entered odd number is :- ")
-        self.number_popping(odd_list)
-        self.user_choice_entry()
+        print(self.number_popping(self.odd_list))
 
     # pop the list and return last entered number    
     def number_popping(self,concerned_list):
         if (len(concerned_list) != 0):
             poped_number = concerned_list.pop()
-            print(poped_number)
-            if poped_number in entered_sequence:
-                entered_sequence.remove(poped_number)
+            self.poped_numbers.append(poped_number)
+            return(poped_number)
         else:
             print("No numbers left in updated list to Display")
-            exit()
     
     # Display sequence of last number entery
     def entered_sequence_display(self):
-        if (len(entered_sequence) != 0):
-            last_entered_number = entered_sequence[-1]
-            print(f"The entered number is {last_entered_number}")
-            entered_sequence.remove(last_entered_number)
+        self.sequence_update()
+        if (len(self.entered_sequence) != 0):
+            print(f"The last number of updated entered sequence is")
+            print(self.number_popping(self.entered_sequence))   
         else:
             print("All the sequence of entered number has been displayed")
-        self.user_choice_entry()
+
+    # Update for sequence list
+    def sequence_update(self):
+        for poped_number in self.poped_numbers:
+            if poped_number in self.entered_sequence:
+                self.entered_sequence.remove(poped_number)
 
     # Obtaining user choice which fountion to execute
     def user_choice_entry(self):
         self.choices_display()
-        while True:
+        while (self.choice != 0):
             if (self.choice == 0):
                 break
             elif (self.choice == 1):
@@ -73,6 +78,7 @@ class list_operations:
                 self.entered_sequence_display()
             else:
                 print("Please Choose among the choice provied")
+            self.choices_display()
         
     # Choice presenter
     def choices_display(self):
