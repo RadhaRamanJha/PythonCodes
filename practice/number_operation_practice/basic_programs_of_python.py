a3ba38c4b1ddb9e71ca25a2849e081b41f03f0b4
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-05-06 19:30:49 +0530

    unittest_and_updated_basic_programs_of_python_geeks_for_geeks

--- practice/number_operation_practice/basic_programs_of_python.py
+++ practice/number_operation_practice/basic_programs_of_python.py
@@ -1,36 +1,56 @@
 """Basic Python Programs excercises from geeksForgeeks 'https://www.geeksforgeeks.org/python-programming-examples/'"""
 class basic_programs():
-    """Return sum of two nummbers"""
-    def numbers_sum(number1,number2):
-        print(f"The sum of two number is {number1+number2}")
-    
-    """Calculation of maximum of two numbers"""
-    def max_number(number1,number2):
-        print(f"The maximum nummber among the two is {max(number1,number2)}")
+    # def __init__(self):
+    #     pass
 
-    """Calculation of factorial for a number"""
-    def factorial(number):
-        if number < 0:
-            return("not calculable for negative numbers")
+    def numbers_sum(self,number1,number2):
+        """Return sum of two nummbers"""
+        if ((type(number1) == int or type(number1) ==  float) and  (type(number2) == int or type(number2) == float) ):
+            sum = number1 + number2
+            return(sum)
         else:
-            if (number == 0 or number == 1):
-                return 1
-            else:
-                return(number*basic_programs.factorial(number-1))
-     
-    """Calculation of Simple intrest"""
-    def simple_interest_calculation(principal_amount,rate,time):
-        simple_intrest = principal_amount*rate*time/100
-        print(f"The Simple Intrest is {simple_intrest}")
+            raise Exception("Missing appropriate numbers type")
+    
+    
+    def max_number(self,number1,number2):
+        """Calculation of maximum of two numbers"""
+        if ((type(number1) == int or type(number1) ==  float) and  (type(number2) == int or type(number2) == float) ): 
+            maximum_number = max(number1,number2)
+            return(maximum_number)
+        else:
+            raise Exception("Missing appropriate numbers type")
 
-    """Calculation of compound interest"""
-    def compound_intrest_calculation(principal_amount,rate,time):
+    
+    def factorial(self,number):
+        """Calculation of factorial for a number"""
+        if type(number) == int :
+            if number < 0:
+                raise Exception("NOT Defined")
+            else:
+                if (number == 0 or number == 1):
+                    return(1)
+                else:
+                    factorial = number*basic_programs.factorial(self,(number-1))
+                    return(factorial)
+        else:
+            raise Exception("Missing appropriate number type")
+     
+    
+    def simple_interest_calculation(self,principal_amount,rate,time):
+        """Calculation of Simple intrest"""
+        simple_intrest = principal_amount*rate*time/100
+        return(simple_intrest)
+
+    
+    def compound_intrest_calculation(self,principal_amount,rate,time):
+        """Calculation of compound interest"""
         future_sum = principal_amount*(1+rate/100)**time
         compound_interest = future_sum - principal_amount
-        print(f"The compound interest is {compound_interest}")
+        return(compound_interest)
     
-    """Check for a number being Armstrong number """
-    def is_armstrong(number):
+    
+    def is_armstrong(self,number):
+        """Check for a number being Armstrong number """
         power= len(str(number))
         sum = 0
         current_number = number
@@ -39,37 +59,43 @@ class basic_programs():
             sum += digit**power
             current_number = ((current_number-digit)//10)
         if (number == sum):
-            print(f"Number {number} is armstrong")
+            return True
         else:
-            print(f"Number {number} is not armstrong")
+            return False
 
-    """Program to find area of a circle"""
-    def find_circle_area(radius_of_circle):
+    
+    def find_circle_area(self,radius_of_circle):
+        """Program to find area of a circle"""
         if ((type(radius_of_circle) == int) 
             or (type(radius_of_circle) == float)):
             if radius_of_circle < 0:
-                print("Incorrect input enter a possitive number")
+                raise ValueError("Not Defined")
             else:
                 Area = 3.14*(radius_of_circle**2)
-                print(f"The area of circle is {Area}")
+                return Area
+        else:
+            raise TypeError("Undefined data type")
 
-    """checking wheter entered number is prime or not"""
-    def is_number_prime(number):
+    
+    def is_number_prime(self,number):
+        """checking wheter entered number is prime or not"""
         for factor in range (2,number):
             if number % factor == 0:
                 return
         return True
     
-    """Return all prime numbers in a given range"""
-    def return_prime_numbers_in(number1,number2):
+    
+    def return_prime_numbers_in(self,number1,number2):
+        """Return all prime numbers in a given range"""
         prime_numbers_in_range = []
         for number in range(number1,number2+1):
             if (basic_programs.is_number_prime(number) == True):
                 prime_numbers_in_range.append(number)
-        print(f"prime numbers in the range are {prime_numbers_in_range}")
+        return prime_numbers_in_range
         
-    """Returns the n-th fibbonaci_number"""
-    def fibbonaci_number(n):
+    
+    def fibbonaci_number(self,n):
+        """Returns the n-th fibbonaci_number"""
         a,b = 0,1
         i = 0 
         while (i < n):
@@ -78,20 +104,21 @@ class basic_programs():
             i+=1
         return fibbonaci_number
     
-    """Checks wheter a number fibbonaci_or_not"""
-    def is_fibbonaci_number(number):
+    
+    def is_fibbonaci_number(self,number):
+        """Checks wheter a number fibbonaci_or_not"""
         a,b = 0,1
         fibbonaci_number = a
         while (fibbonaci_number <= number):
             if (fibbonaci_number == number):
-                print(f"{number} is a fibbonaci number")
-                break
+                return True
             fibbonaci_number = b
             a,b = b,a+b
-        print(f"{number} is not a fibbonaci number")
+        return False
     
-    """Returns position of kth multiple of 'n' in fibbonaci-series"""
-    def position_in_fibbonaci_series(n,k):
+    
+    def position_in_fibbonaci_series(self,n,k):
+        """Returns position of kth multiple of 'n' in fibbonaci-series"""
         a,b,position = 1,1,1
         current_fibbonaci_number = a
         current_k_value = 0
@@ -104,33 +131,20 @@ class basic_programs():
             current_fibbonaci_number = b
             a,b = b,a+b
 
-    """Calculates sum of sqares of first 'n' natural numbers"""
-    def square_sum_of_n_natural_numbers(n):
+    
+    def square_sum_of_n_natural_numbers(self,n):
+        """Calculates sum of sqares of first 'n' natural numbers"""
         natural_number,square,sum = 1,1,1
         for natural_number in range (2,n+1):
             square = natural_number**2
             sum += square
         return(sum)
     
-    """Calculates sum of cubes of first 'n' natural numbers"""
-    def cube_sum_of_n_natural_numbers(n):
+    
+    def cube_sum_of_n_natural_numbers(self,n):
+        """Calculates sum of cubes of first 'n' natural numbers"""
         natural_number,cube,sum = 1,1,1
         for natural_number in range (2,n+1):
             cube = natural_number**3
             sum += cube
         return(sum)
-
-basic_programs.numbers_sum(467,560)
-basic_programs.max_number(75,77)
-print(f"The factorial of number is {basic_programs.factorial(-1)}")
-basic_programs.simple_interest_calculation(500000,10,8)
-basic_programs.compound_intrest_calculation(500000,10,8)
-basic_programs.find_circle_area(100)
-basic_programs.is_armstrong(1634)
-print(f"67 is prime {basic_programs.is_number_prime(67)}")
-basic_programs.return_prime_numbers_in(2,234)
-print(f"The 11th fibbonaci number is {basic_programs.fibbonaci_number(11)}")
-basic_programs.is_fibbonaci_number(12)
-print(f"The sum of square of first 2 natural numbers is {basic_programs.square_sum_of_n_natural_numbers(2)}")
-print(f"The sum of cube of first 2 natural numbers is {basic_programs.cube_sum_of_n_natural_numbers(2)}")
-print(f"The postion of 2nd multiple of 4 in fibbonaci series is {basic_programs.position_in_fibbonaci_series(4,2)}")
---
+++ practice/number_operation_practice/unit_test_basic_programs_of_python.py
@@ -0,0 +1,96 @@
+import unittest
+from basic_programs_of_python import basic_programs
+
+class BasicProgramTest(unittest.TestCase):
+    """Unit Tests for entire basic_program class"""
+    def setUp(self):
+       """Creates an instance of basic programs (bp)"""
+       self.bp = basic_programs()
+
+    def test_numbers_sum(self):
+        """A Test to verify normal behaviour of  'numbers_sum' function"""
+        self.assertEqual(9,self.bp.numbers_sum(4,5))
+    def test_number_sum_str_args(self):
+        """Test to verify exception being thrown by 'numbers_sum' function"""
+        self.assertRaises(Exception,self.bp.numbers_sum,('A','B'))
+
+    def test_max_number(self):
+        """A Test to verify normal behaviour of  'max_number' function"""
+        self.assertEqual(108,self.bp.max_number(102,108))
+    def test_max_number_str_args(self):
+        """Test to verify exception being thrown by 'max_number' function"""
+        self.assertRaises(Exception,self.bp.max_number,('a','d'))
+
+    def test_factorial(self):
+        """A test to verify normal behaviour of 'factorial' function for 0 value"""
+        self.assertEqual(1,self.bp.factorial(0))
+    def test_factorial_negative_args(self):
+        """Test to verify exception being thrown by 'factorial' function' for -ve arguments"""
+        self.assertRaises(Exception,self.bp.factorial,-1)
+    def test_factorial_str_args(self):
+        """Test to verify exception being thrown by 'factorial' function' for str arguments"""
+        self.assertRaises(Exception,self.bp.factorial,'a')
+    def test_factorial_(self):
+        """A test to verify normal behaviour of 'factorial' function"""
+        self.assertEqual(120,self.bp.factorial(5))
+
+    def test_simple_interest_calculation(self):
+        """Checks the calculation of Simple interest"""
+        self.assertEqual(1000,self.bp.simple_interest_calculation(10000,5,2))
+    
+    def test_compound_intrest_calculation(self):
+        """Checks the calculation of Simple interest"""
+        self.assertEqual(1025,self.bp.compound_intrest_calculation(10000,5,2))
+    
+    def test_is_armstrong(self):
+        """Checks the test for armmstrong number"""
+        self.assertTrue(self.bp.is_armstrong(153))
+        
+    def test_is_armstrong_returns_false(self):
+        """Checks the test for armstrong number fails"""
+        self.assertFalse(self.bp.is_armstrong(152))
+
+    def test_find_circle_area(self):
+        """Checks the area of circle"""
+        self.assertEqual(314,self.bp.find_circle_area(10))
+
+    def test_negative_radius_exception_circle_area(self):
+        """Checks the return in case of negative radius"""
+        self.assertRaises(ValueError,self.bp.find_circle_area,-23)
+
+    def test_string_radius_error_circle_area(self):
+        """Checks the retuen in case string value of radius provided"""
+        self.assertRaises(TypeError,self.bp.find_circle_area,'abc')
+    
+    def test_is_number_prime(self):
+        """Verifies the prime number is detected"""
+        self.assertTrue(self.bp.is_number_prime(7))
+
+    def test_fibbonaci_number(self):
+        """Test the return of correct fibbonaci number"""
+        self.assertEqual(8,self.bp.fibbonaci_number(7))
+
+    def test_is_fibbonaci_number(self):
+        """Tests fibbonaci number wheter fibbonaci number being detected """
+        self.assertTrue(self.bp.is_fibbonaci_number(8))
+    
+    def test_is_not_fibbonaci_number(self):
+        """Tests fibbonaci number wheter fibbonaci number being detected """
+        self.assertFalse(self.bp.is_fibbonaci_number(9))
+    
+    def test_position_in_fibbonaci_series(self):
+        """Checks if true position of number in fibbonaci series being returned"""
+        self.assertEqual(7,self.bp.position_in_fibbonaci_series(2,2))
+    
+    def test_square_sum_of_n_natural_numbers(self):
+        """Checks wheter true sum of squares being returned"""
+        self.assertEqual(5,self.bp.square_sum_of_n_natural_numbers(2))
+    
+    def test_cube_sum_of_n_natural_numbers(self):
+        """Checks wheter true sum of cubes being returned"""
+        self.assertEqual(9,self.bp.cube_sum_of_n_natural_numbers(2))
+
+
+        
+if __name__ == '__main__':
+    unittest.main()
