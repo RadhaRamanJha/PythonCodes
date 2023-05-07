39d78d7946144ece61f736b063141731996448c0
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-05-07 19:58:30 +0530

    updating moved files

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-def swap_list(new_list):
-    
--- tuple.py
+++
@@ -1,6 +0,0 @@
-# if comma is not placed after first element it is considered as a string in python
-tuple = ("python")*3
-print(tuple)
-# first element should be seperated by comma to tell python it is tuple
-tuple = ("python",)*3
-print(tuple)
