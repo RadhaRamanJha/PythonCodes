8dce9a5f099b2e6b8ee1b44e8488f55dd905909b
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-05-24 11:31:28 +0530

    deleting moved files

--- module3.5,6,7---tuple(slicing,converting in list, counting).py
+++
@@ -1,9 +0,0 @@
-tuple1 = ("Python", 1, 2, 3, "Java", "A", "B")
-n = len(tuple1)
-# printing alternate values of tuple
-print(tuple1[0:n:2])
-# printing value between index 1 to 5
-print(tuple1[1:5])
-# printing last value of tuple
-print(tuple1[-1])
-
