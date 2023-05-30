84180e2db67bb001c84700a8b2e3c9550c88a1b2
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-05-30 14:34:53 +0530

    modified_basic_programs_and_unit_tests

--- practice/number_operation_practice/basic_programs_of_python.py
+++ practice/number_operation_practice/basic_programs_of_python.py
@@ -4,17 +4,17 @@ class basic_programs():
          pass
     def numbers_sum(self,number1,number2):
         """Return sum of two nummbers"""
-        if (type(number1) == int or type(number1) ==  float):
-                if(type(number2) == int or type(number2) == float):
-                    sum = number1 + number2
-                    return(sum)
+        if ((type(number1) == int or type(number1) ==  float) and
+             (type(number2) == int or type(number2) == float)):
+                sum = number1 + number2
+                return(sum)
         else:
             raise Exception("Missing appropriate numbers type")
     
     def max_number(self,number1,number2):
         """Calculation of maximum of two numbers"""
-        if (type(number1) == int or type(number1) ==  float):
-            if(type(number2) == int or type(number2) == float): 
+        if ((type(number1) == int or type(number1) ==  float) and
+             (type(number2) == int or type(number2) == float)): 
                 maximum_number = max(number1,number2)
                 return(maximum_number)
         else:
@@ -35,17 +35,13 @@ class basic_programs():
     
     def simple_interest_calculation(self,principal_amount,rate,time):
         """Calculation of Simple intrest"""
-        if (type(principal_amount) == int) or (type(principal_amount) == float):
-            if(type(time) == int) or (type(time) == float):
-                if(type(rate) == float or type(rate) == int):
-                    simple_intrest = principal_amount*rate*time/100
-                    return(simple_intrest)
-                else:
-                    raise Exception("Missing appropriate rate data type")
-            else:
-                raise Exception("Missing appropriate time data type")
+        if ((type(principal_amount) == int) or (type(principal_amount) == float) and
+            (type(time) == int) or (type(time) == float) and 
+            (type(rate) == float or type(rate) == int)):
+                simple_intrest = principal_amount*rate*time/100
+                return(simple_intrest)
         else:
-            raise Exception("Missing appropriate principal data type")
+            raise Exception("Missing appropriate data type")
 
     def compound_intrest_calculation(self,principal_amount,rate,time):
         """Calculation of compound interest"""
@@ -61,8 +57,7 @@ class basic_programs():
                 raise Exception("Missing appropriate time data type")
         else:
             raise Exception("Missing appropriate principal data type")
-    
-    
+      
     def is_armstrong(self,number):
         """Check for a number being Armstrong number """
         if (type(number)==int):
@@ -79,8 +74,7 @@ class basic_programs():
                 return False
         else:
             raise Exception("Missing appropriate number type")
-
-    
+ 
     def find_circle_area(self,radius_of_circle):
         """Program to find area of a circle"""
         if ((type(radius_of_circle) == int) 
@@ -92,7 +86,6 @@ class basic_programs():
                 raise ValueError("Not Defined")
         else:
             raise Exception("Missing appropriate radius type")
-
     
     def is_number_prime(self,number):
         """checking wheter entered number is prime or not"""
@@ -107,16 +100,14 @@ class basic_programs():
         else:
             raise Exception("Missing appropriate numbers type")
     
-    
     def return_prime_numbers_in(self,number1,number2):
         """Return all prime numbers in a given range"""
         prime_numbers_in_range = []
-        if type(number1)==int :
-            if type(number2)==int:
-                for number in range(number1,number2+1):
-                    if (self.is_number_prime(number) == True):
-                        prime_numbers_in_range.append(number)
-                return prime_numbers_in_range
+        if type(number1)==int and type(number2)==int:
+            for number in range(min(number1,number2),max(number1,number2)+1):
+                if (self.is_number_prime(number) == True):
+                    prime_numbers_in_range.append(number)
+            return prime_numbers_in_range
         else:
             raise Exception("Missing appropriate numbers type")
             
@@ -125,25 +116,32 @@ class basic_programs():
         a,b = 0,1
         i = 0
         if (type(n) == int): 
-            while (i < n):
-                fibbonaci_number = a
-                a,b = b,a+b
-                i+=1
-            return fibbonaci_number
+            if (n>0):
+                while (i < n):
+                    fibbonaci_number = a
+                    a,b = b,a+b
+                    i+=1
+                return fibbonaci_number
+            else:
+                raise ValueError("Series not applicable for negative input")
         else:
-            raise Exception("Missing appropriate number type")
+            raise Exception("Missing appropriate data type type")
     
     def is_fibbonaci_number(self,number):
-        """Checks wheter a number fibbonaci_or_not"""
+        """Checks wheter a number fibbonaci_or_not by eqauting
+           current fibbonaci number to the argument number provided"""
         if type(number)==int:
-            a,b = 0,1
-            fibbonaci_number = a
-            while (fibbonaci_number <= number):
-                if (fibbonaci_number == number):
-                    return True
-                fibbonaci_number = b
-                a,b = b,a+b
-            return False
+            if (number >=0): 
+                a,b = 0,1
+                fibbonaci_number = a
+                while (fibbonaci_number <= number):
+                    if (fibbonaci_number == number):
+                        return True
+                    fibbonaci_number = b
+                    a,b = b,a+b
+                return False
+            else:
+                raise ValueError('Missing appropriate value of \'number\'')
         else:
             raise Exception("Missing appropriate number type")
        
@@ -155,13 +153,15 @@ class basic_programs():
                 current_fibbonaci_number = a
                 current_k_value = 0
                 while (current_k_value <= k):
+                    current_fibbonaci_number = b
+                    a,b = b,a+b
                     position +=1
                     if (current_fibbonaci_number % n == 0):
                         current_k_value += 1
                         if(current_k_value == k):
-                            return position
-                    current_fibbonaci_number = b
-                    a,b = b,a+b
+                            return position                   
+            else:
+                raise(TypeError('Missing appropriate position type'))
         else:
             raise Exception("Missing appropriate number type")
    
@@ -173,12 +173,11 @@ class basic_programs():
                 for natural_number in range (2,n+1):
                     square = natural_number**2
                     sum += square
-                return(sum)
             elif (n == 0):
                 sum = 0
-                return sum
             elif (n < 0):
-                raise Exception("Negative numbers are not considered as natural numbers")
+                raise ValueError("Negative numbers are not considered as natural numbers")
+            return(sum)
         else:
            raise Exception("Missing appropriate number type") 
        
@@ -195,6 +194,6 @@ class basic_programs():
                 sum = 0
                 return sum
             elif n < 0:
-                Exception("Negative numbers are not considered as natural numbers")
+                raise ValueError("Negative numbers are not considered as natural numbers")
         else:
             raise Exception("Missing appropriate number type")            
--- practice/number_operation_practice/unit_test_basic_programs_of_python.py
+++ practice/number_operation_practice/unit_test_basic_programs_of_python.py
@@ -2,107 +2,189 @@ import unittest
 from basic_programs_of_python import basic_programs
 
 class BasicProgramTest(unittest.TestCase):
-    # def setUpClass(self):
-    #    """Creates an instance of basic programs (bp)"""
-    #    print("setUp Class called")
-    # def tearDownClass(self):
-    #    """Creates an instance of basic programs (bp)"""
-    #    print("tearDown Class called\n")
     """Unit Tests for entire basic_program class"""
     def setUp(self):
        """Creates an instance of basic programs (bp)"""
-    #    print("setup called")
        self.bp = basic_programs()
-    # def tearDown(self):
-       """Creates an instance of basic programs (bp)"""
-       print("tearDown called\n")
 
+    """Unit tests for sum of two numbers"""
     def test_numbers_sum(self):
-        """A Test to verify normal behaviour of  'numbers_sum' function"""
+        """1.verify normal behaviour of  'numbers_sum' function"""
         self.assertEqual(9,self.bp.numbers_sum(4,5))
     def test_number_sum_str_args(self):
-        """Test to verify exception being thrown by 'numbers_sum' function"""
-        self.assertRaises(Exception,self.bp.numbers_sum,('A','B'))
+        """2.verify exception being thrown by 'numbers_sum' function"""
+        self.assertRaises(Exception,self.bp.numbers_sum,'A','B')
 
+    """Unit tests for finding maximum of two numbers"""
     def test_max_number(self):
-        """A Test to verify normal behaviour of  'max_number' function"""
+        """3.verify normal behaviour of  'max_number' function"""
         self.assertEqual(108,self.bp.max_number(102,108))
     def test_max_number_str_args(self):
-        """Test to verify exception being thrown by 'max_number' function"""
-        self.assertRaises(Exception,self.bp.max_number,('a','d'))
+        """4.Test to verify exception being thrown by 'max_number' function"""
+        self.assertRaises(Exception,self.bp.max_number,'a','d')
 
+    """Unit test for factorial of numbers"""
     def test_factorial(self):
-        """A test to verify normal behaviour of 'factorial' function for 0 value"""
+        """5.A test to verify normal behaviour of 'factorial' function for 0 value"""
         self.assertEqual(1,self.bp.factorial(0))
     def test_factorial_negative_args(self):
-        """Test to verify exception being thrown by 'factorial' function' for -ve arguments"""
+        """6.Test to verify exception being thrown by 'factorial' function' for -ve arguments"""
         self.assertRaises(Exception,self.bp.factorial,-1)
     def test_factorial_str_args(self):
-        """Test to verify exception being thrown by 'factorial' function' for str arguments"""
-        self.assertRaises(Exception,self.bp.factorial,'a')
-    def test_factorial_(self):
-        """A test to verify normal behaviour of 'factorial' function"""
-        self.assertEqual(120,self.bp.factorial(5))
+        """7.Test to verify exception being thrown by 'factorial' function' for str arguments"""
+        self.assertRaises(TypeError,self.bp.factorial,'a')
 
+    """Unit tests for simple interest calculation"""
     def test_simple_interest_calculation(self):
-        """Checks the calculation of Simple interest"""
+        """8.Checks the calculation of Simple interest"""
         self.assertEqual(1000,self.bp.simple_interest_calculation(10000,5,2))
-    
+    def test_simple_interest_exception_principal(self):
+        """9.Checks the simple interest function returns error for string principal value"""
+        self.assertRaises(Exception,self.bp.simple_interest_calculation,'p',5,2)
+    def test_simple_interest_exception_time(self):
+        """10.Checks the simple interest function returns error for string time value"""
+        self.assertRaises(Exception,self.bp.simple_interest_calculation,10000,5,'2')
+    def test_simple_interest_exception_rate(self):
+        """11.Checks the simple interest function returns error for string rate value"""
+        self.assertRaises(Exception,self.bp.simple_interest_calculation,10000,'5',2)
+
+    """Unit tests for compound interest calculation"""
     def test_compound_intrest_calculation(self):
-        """Checks the calculation of Simple interest"""
+        """12.Checks the calculation of compound interest"""
         self.assertEqual(1025,self.bp.compound_intrest_calculation(10000,5,2))
-    
+    def test_compound_interest_exception_principal(self):
+        """13.Checks the compound interest function returns error for string principal value"""
+        self.assertRaises(Exception,self.bp.compound_intrest_calculation,'10000',5,2)
+    def test_compound_interest_exception_time(self):
+        """14.Checks the compound interest function returns error for string time value"""
+        self.assertRaises(Exception,self.bp.compound_intrest_calculation,10000,5,'2')
+    def test_compound_interest_exception_rate(self):
+        """15.Checks the compound interest function returns error for string rate value"""
+        self.assertRaises(Exception,self.bp.compound_intrest_calculation,10000,'5',2)
+
+    """Unit tests for is armstrong numbers verification"""    
     def test_is_armstrong(self):
-        """Checks the test for armmstrong number"""
-        self.assertTrue(self.bp.is_armstrong(153))
-        
+        """16.Checks the test for armmstrong number"""
+        self.assertTrue(self.bp.is_armstrong(153)) 
     def test_is_armstrong_returns_false(self):
-        """Checks the test for armstrong number fails"""
+        """17.Checks the test for armstrong number fails"""
         self.assertFalse(self.bp.is_armstrong(152))
-
+    def test_is_armstrong_returns_exception_float(self):
+        """18.Checks the test for armstrong number returns exception for float"""
+        self.assertRaises(Exception,self.bp.is_armstrong,12.5)
+    def test_is_armstrong_returns_exception_string(self):
+        """19.Checks the test for armstrong number returns exception for string"""
+        self.assertRaises(Exception,self.bp.is_armstrong,'153')
+    
+    """Unit tests for circle area calulation"""
     def test_find_circle_area(self):
-        """Checks the area of circle"""
+        """20.Checks function returns correct area of circle"""
         self.assertEqual(314,self.bp.find_circle_area(10))
-
-    def test_negative_radius_exception_circle_area(self):
-        """Checks the return in case of negative radius"""
-        self.assertRaises(ValueError,self.bp.find_circle_area,-23)
-
-    def test_string_radius_error_circle_area(self):
-        """Checks the retuen in case string value of radius provided"""
-        self.assertRaises(TypeError,self.bp.find_circle_area,'abc')
-    
+    def test_find_circle_area_float(self):
+        """21.Checks function returns correct area of circle float radius"""
+        self.assertEqual(346.185,self.bp.find_circle_area(10.5))
+    def test_circle_area_value_error(self):
+        """22.Checks function raises value error for negative circle radius"""
+        self.assertRaises(ValueError,self.bp.find_circle_area,-10)
+    def test_circle_area_exception(self):
+        """23.Checks function raises exception for string circle radius"""
+        self.assertRaises(Exception,self.bp.find_circle_area,'10')
+        
+    """Unit tests for is_prime"""
     def test_is_number_prime(self):
-        """Verifies the prime number is detected"""
+        """24.Verifies the prime number is detected"""
         self.assertTrue(self.bp.is_number_prime(7))
+    def test_number_not_prime(self):
+        """25.Verifies non prime number returns false"""
+        self.assertFalse(self.bp.is_number_prime(6))
+    def test_0_not_prime(self):
+        """26.Verifies 0 is returned as non-prime nummber"""
+        self.assertFalse(self.bp.is_number_prime(0))
+    def test_1_not_prime(self):
+        """27.Verifies 1 is returned as non-prime nummber"""
+        self.assertFalse(self.bp.is_number_prime(1))
+    def test_str_not_prime(self):
+        """28.Verifies 'str' input throws an exception"""
+        self.assertRaises(Exception,self.bp.is_number_prime,'12')
 
-    def test_fibbonaci_number(self):
-        """Test the return of correct fibbonaci number"""
+    """Unit tests for fibbonaci number"""
+    def test_fibbonaci_num(self):
+        """29.Test the return of correct fibbonaci number"""
         self.assertEqual(8,self.bp.fibbonaci_number(7))
+    def test_fibbonani_neg_num(self):
+        """30.Test to check function raises value error for negative arg num"""
+        self.assertRaises(ValueError,self.bp.fibbonaci_number,-1)
+    def test_fibbo_float_num(self):
+        """31.Checks function raises value error for float num"""
+        self.assertRaises(Exception,self.bp.fibbonaci_number,1.2)
+    def test_fibbonani_str_num(self):
+        """32.Checks function raises exception for str arg num"""
+        self.assertRaises(Exception,self.bp.fibbonaci_number,'1')
 
+    """Unit tests for checking number is fibbonaci"""
     def test_is_fibbonaci_number(self):
-        """Tests fibbonaci number wheter fibbonaci number being detected """
+        """33.Tests fibbonaci number wheter fibbonaci number being detected """
         self.assertTrue(self.bp.is_fibbonaci_number(8))
-    
     def test_is_not_fibbonaci_number(self):
-        """Tests fibbonaci number wheter fibbonaci number being detected """
+        """34.Tests fibbonaci number wheter fibbonaci number being detected """
         self.assertFalse(self.bp.is_fibbonaci_number(9))
+    def test_is_fibbo_raise_execption_neg_number(self):
+        """35.Checks is_fibbo function raises exception for negative number"""
+        self.assertRaises(ValueError,self.bp.is_fibbonaci_number,-1)
+    def test_is_fibbo_raise_execption_str_number(self):
+        """36.Checks is_fibbo function raises exception for str number"""
+        self.assertRaises(Exception,self.bp.is_fibbonaci_number,'1')
     
+    """Unit tests for determing position of 'k'th multiple of 'n' in fibbo series"""
     def test_position_in_fibbonaci_series(self):
-        """Checks if true position of number in fibbonaci series being returned"""
+        """37.Checks if true position of number in fibbonaci series being returned"""
         self.assertEqual(7,self.bp.position_in_fibbonaci_series(2,2))
-        print("test fibo test")
+    def test_pos_fibbo_raise_typeerror_float_k(self):
+        """38.Position in fibbo raises type error for float k value"""
+        self.assertRaises(TypeError,self.bp.position_in_fibbonaci_series,7,2.5)
+    def test_pos_fibbo_raise_typeerror_str_k(self):
+        """39.Position in fibbo raises type error for str k value"""
+        self.assertRaises(TypeError,self.bp.position_in_fibbonaci_series,7,'2')
+    def test_pos_fibbo_raise_typeerror_float_n(self):
+        """40.Position in fibbo raises exception for float n value"""
+        self.assertRaises(Exception,self.bp.position_in_fibbonaci_series,7.2,2)
+    def test_pos_fibbo_raise_typeerror_str_n(self):
+        """41.Position in fibbo raises exception for str n value"""
+        self.assertRaises(Exception,self.bp.position_in_fibbonaci_series,'7',2)
     
+    """Unit tests for square sum of 'n' natural numbers"""
     def test_square_sum_of_n_natural_numbers(self):
-        """Checks wheter true sum of squares being returned"""
+        """42.Checks wheter true sum of squares being returned"""
         self.assertEqual(5,self.bp.square_sum_of_n_natural_numbers(2))
-        print("test square test")
+    def test_sq_sum_returns_value_error_neg_num(self):
+        """43.Checks sum of square returns value error for neg num"""
+        self.assertRaises(ValueError,self.bp.square_sum_of_n_natural_numbers,-23)
+    def test_sq_sum_returns_0_num(self):
+        """44.Verifies 0 as sum for 0 natural numbers"""
+        self.assertEqual(0,self.bp.square_sum_of_n_natural_numbers(0))
+    def test_sq_sum_raises_exception_for_float_input(self):
+        """45.Verifies excetion raised for float number enterd"""
+        self.assertRaises(Exception,self.bp.square_sum_of_n_natural_numbers,2.3)
+    def test_sq_sum_raises_exception_for_str_input(self):
+        """46.Verifies excetion raised for str number enterd"""
+        self.assertRaises(Exception,self.bp.square_sum_of_n_natural_numbers,'2')
     
+    """Unit tests for cube sum of 'n' natural numbers"""
     def test_cube_sum_of_n_natural_numbers(self):
-        """Checks wheter true sum of cubes being returned"""
+        """47.Checks wheter true sum of cubes being returned"""
         self.assertEqual(9,self.bp.cube_sum_of_n_natural_numbers(2))
-        print("test cube test")
-
+    def test_cube_n_nat_num_0(self):
+        """48.Verifies 0 is sum for adding 0 nat num"""
+        self.assertEqual(0,self.bp.cube_sum_of_n_natural_numbers(0))
+    def test_cube_neg_n_nat_num(self):
+        """49.Verifies value error raised for nat num entered as negative value"""
+        self.assertRaises(ValueError,self.bp.cube_sum_of_n_natural_numbers,-3)
+    def test_cube_float_n_nat_numm(self):
+        """50.Verifies exception raised for float num entered"""
+        self.assertRaises(Exception,self.bp.cube_sum_of_n_natural_numbers,2.5)
+    def test_cube_str_n_nat_numm(self):
+        """51.Verifies exception raised for str num entered"""
+        self.assertRaises(Exception,self.bp.cube_sum_of_n_natural_numbers,'34')
 
 if __name__ == '__main__':
     unittest.main()
