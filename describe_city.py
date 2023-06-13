8d095c940bbbd16925b98897f48dfa653516501f
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-06-13 21:15:41 +0530

    delete for Refactoring

--- describe_city.py
+++
@@ -1,8 +0,0 @@
-def describe_city(city = "Nagpur", country = "India"):
-    """Describes the Geo-graphic loacation of Country"""
-    print(f"{city} City is in the Country {country}")
-
-describe_city("Reykjavik","Iceland")
-describe_city("Melborne","Australia")
-describe_city("London","Britain")
-describe_city()
