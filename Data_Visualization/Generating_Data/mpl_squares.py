7ff161e82d8427a14526edacd79a14bfc8d1b210
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-08-01 07:18:45 +0530

    update for python program

--- Data_Visualization/Generating_Data/mpl_squares.py
+++ Data_Visualization/Generating_Data/mpl_squares.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
 
-values = [1,2,3,4,5]
-
-square_values = [1,4,9,16,25]
+square_values = [value**2 for value in range(0,11)]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(values,square_values, linewidth = 3)
+ax.plot(square_values, linewidth = 5)
 
 """Set Chart title and label axes"""
 ax.set_title("Square Numbers",fontsize = 24)
@@ -14,6 +12,6 @@ ax.set_xlabel("Value",fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
 
 # Set the size of tick labels
-ax.tick_params(axis="both",labelsize =14)
+ax.tick_params(axis="both",labelsize =15)
 
 plt.show()
---
+++ practice/patterns/input_practice.py
@@ -0,0 +1,30 @@
+def input_call():
+    print("What is your name ?")
+    name = input('> ')
+    print("What year you were born ?")
+    birthyear = int(input('> '))
+    print("Hi %s You are %d years old" %(name, 2023-birthyear))
+# var1 = int(input("> "))
+# if var1:
+#     print(True)
+# else:
+#     print("False")
+
+
+"""Folowing code is wrong correct it"""
+
+# def it_calculation():
+#     print("Enter your salary")
+#     salary = int(input('> '))
+#     r = 0 
+#     if 100000 <= salary < 300000:
+#         r = 0
+#     elif 300000 <= salary < 500000:
+#         r = 5 
+#     elif 500000 <= salary < 1000000:
+#         r = 10
+#     else:
+#         r = 20
+#     incm_tax = salary*r/100
+#     print(f"Your income tax is {incm_tax}") 
+# it_calculation()
