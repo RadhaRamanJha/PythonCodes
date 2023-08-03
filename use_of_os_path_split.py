9f1d3d4f1a04f3000b238a3d30e82ea2497c196e
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-08-03 18:52:11 +0530

    next delete node

--- use_of_os_path_split.py
+++
@@ -1,4 +0,0 @@
-import os
-_,filename = os.path.split("OneDrive\Desktop\Python_notes_learnvern")
-print(f"Path and the name of file are respectivlely \t{_},\t{filename}")
-
--- using_lambda.py
+++
@@ -1,3 +0,0 @@
-a = int(input("Enter the first number :"))
-b = int(input("Enter the second number :"))
-(lambda a,b:print(a+b))(a,b)
