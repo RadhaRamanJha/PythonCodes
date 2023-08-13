ef2fa9800a92a36f5f42a1f6a35c5ad53bdf3358
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-08-13 22:14:11 +0530

    delete for modification

--- import_privileges_admin_inheritence.py
+++
@@ -1,6 +0,0 @@
-from privileges_admin_inheritence import (User, Privileges,Admin)
-admin1 = Admin("Vikas","Singh")
-admin1.emailID = "Vikas.singh@gmail.com"
-admin1.greet_user
-admin1.describe_user
-print(f"The privilages given to {admin1.first_name} {admin1.last_name} are {admin1.privileges.show_privileges()}")
--- import_resturant_modified.py
+++
@@ -1,4 +0,0 @@
-from resturant_modified import Resturant as Res
-my_resturant = Res("Flavours","South Indian")
-my_resturant.describe_resturant()
-my_resturant.open_resturant()
