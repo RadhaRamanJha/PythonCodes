dd9c5a4c557b0be62d74a9475c98bea09bb39738
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-08-30 07:31:00 +0530

    delete for rearrangment

--- name_founction.py
+++
@@ -1,6 +0,0 @@
-def get_formatted_name(first_name, last_name, middle_name = ""):
-    if middle_name:
-        formatted_name =  f"{first_name} {middle_name} {last_name}"
-    else:
-        formatted_name = f"{first_name} {last_name}"
-    return formatted_name.title()
