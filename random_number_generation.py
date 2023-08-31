2f90bf3de2de05e2bb2d713397e3475ee4b08995
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-08-31 10:10:53 +0530

    delete to reaarange

--- random_number_generation.py
+++
@@ -1,4 +0,0 @@
-'''Generating a random number between 1000 to 5000'''
-from random import randint
-generated_random_number = randint(1000,5000)
-print(f"The Random number generated is : {generated_random_number}")
