1d2beeab10c7ac85d886055d261e73203de2527d
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-01-04 18:21:45 +0530

    Repository Clean up

--- Python_basics/revision_exersises/variab_datatype/even_odd_determination.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter a number : " ))
-b = a % 2
-if b == 0 :
-    print ("The number {} is even".format(a))
-else:
-    print("The number {} is odd".format(a))
-# list play
-cubes = list(value**3 for value in range(1,101,2))
-print(cubes[11:22:2])
--- Python_basics/revision_exersises/variab_datatype/number_addition.py
+++
@@ -1,3 +0,0 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-print(f"The sum of the entered numbers is {a+b}")
