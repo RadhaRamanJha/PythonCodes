a0deeb6669fd2c09eb2c53289179460ac75b9560
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-01-10 19:36:38 +0530

    Cleaning up the old programs

--- Python_basics/practice/strings_practice/string_programs.py
+++
@@ -1,8 +0,0 @@
-"""string exersises given on geeks for geeks 
-https://www.geeksforgeeks.org/python-programming-examples/#string"""
-class string_operations():
-    def str_palindrome():
-        print("Enter a string to check wheter it's palindrome or not")
-        inp_str = input('> ')
-       
-string_operations.str_palindrome()
--- Python_basics/practice/strings_practice/unused_string_operations.py
+++
@@ -1,9 +0,0 @@
-letter_template = """\t\tThis program by radha is completed on 28-04-2023, 
-                    program\'s uploading date is fixed by radha on 28-04-2023,
-                    even though it may simply seem a daily routine, but.....
-                    It is what it is, It is actually a daily routine of future """
-operation = input("Enter the new operation: ")
-name = input("Enter new  coder name: ")
-date = input("Enter the uploading date: ")
-action = input("Enter an approprtiate action")
-print((((letter_template.replace('radha',name)).replace('28-04-2023',date)).replace('program',operation)).replace('daily routine',action))
