4b2c78b1152190ec0fc5f996a3da7f00837c891a
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-02-15 23:33:19 +0530

    Random Forest Modification

--- Python_basics/linked_list_and_array/sorted_array.py
+++
@@ -1,6 +0,0 @@
-def isArrayInSortedOrder(A):
-    if len(A) == 1 :
-        return True
-    return A[0] <= A[1] and isArrayInSortedOrder(A[1:])
-A = [127, 200, 246, 333, 321, 454, 533, 565, 933]
-print(isArrayInSortedOrder(A))
